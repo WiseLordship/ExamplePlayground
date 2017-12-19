ca66e3e5d4215553819812978605666358fb7f99
Lord <ION3395@absg.net>
2017-12-19 16:19:55 -0600

    Adding string reversal example

---
+++ ExamplePlaygroundTests/StringManipulationTests.cs
@@ -0,0 +1,25 @@
+﻿using ExamplePlayground;
+using FluentAssertions;
+using Xunit;
+
+namespace ExamplePlaygroundTests
+{
+    public class StringManipulationTests
+    {
+        [Theory]
+        [InlineData("","")]
+        [InlineData(null,null)]
+        [InlineData("A","A")]
+        [InlineData("b","b")]
+        [InlineData("CC","CC")]
+        [InlineData("Cd","dC")]
+        [InlineData("ABC","CBA")]
+        [InlineData("Help","pleH")]
+        [InlineData("Thisisalongseentencetotestlength", "htgneltsetotecnetneesgnolasisihT")]
+        public void StringManipulationTest(string input, string expected)
+        {
+            var result = StringManipulation.ReverseString(input);
+            result.Should().Be(expected);
+        }
+    }
+}
