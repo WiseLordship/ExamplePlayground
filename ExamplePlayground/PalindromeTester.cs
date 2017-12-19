24d23b9a68a47912be75c7c02ac7a57676ffe609
Lord <ION3395@absg.net>
2017-12-19 15:33:27 -0600

    Adding Palindrome example

---
+++ ExamplePlayground/PalindromeTester.cs
@@ -0,0 +1,34 @@
+﻿namespace ExamplePlayground
+{
+    public static class PalindromeTester
+    {
+        public static bool Check(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            if(input.Length < 2)
+            {
+                return true;
+            }
+
+            var inputLower = input.ToLower();
+
+            for(var index = 0; index < input.Length; index++)
+            {
+                var endMarker = (inputLower.Length-1) - index;
+
+                if(index == endMarker) { return true; }
+
+                if(inputLower[index] != inputLower[endMarker])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ExamplePlaygroundTests/PalindromeTesterTests.cs
@@ -0,0 +1,26 @@
+﻿using ExamplePlayground;
+using FluentAssertions;
+using Xunit;
+
+namespace ExamplePlaygroundTests
+{
+    public class PalindromeTesterTests
+    {
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("AB", false)]
+        [InlineData("A", true)]
+        [InlineData("ABA", true)]
+        [InlineData("ABBA", true)]
+        [InlineData("ABABA", true)]
+        [InlineData("TacocaT", true)]
+        [InlineData("tAcOCaT", true)]
+        [InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZZYXWVUTSRQPONMLKJIHGFEDCBA", true)]
+        public void Tests(string input, bool expected)
+        {
+            var result = PalindromeTester.Check(input);
+            result.Should().Be(expected);
+        }
+    }
+}
