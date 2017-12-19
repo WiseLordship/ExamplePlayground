1ea1381f2ac3643447f5f99e617b503c31536220
Lord <ION3395@absg.net>
2017-12-19 15:15:41 -0600

    Adding base and IntParse Example

---
+++ ExamplePlayground/IntParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExamplePlayground
+{
+    public class IntParser
+    {
+        public static int Parse(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new Exception("Not valid input");
+            }
+
+            bool isNegative = false;
+            if (input[0] == '-')
+            {
+                isNegative = true;
+                input = input.Substring(1);
+            }
+
+            if (!Regex.IsMatch(input, "^[0-9]*$"))
+            {
+                throw new Exception("Not valid input");
+            }
+
+            int results = 0;
+
+            if (input.Length == 1)
+            {
+                results = (int)Char.GetNumericValue(input[0]);
+            }
+            else
+            {
+                for (var index = (isNegative ? 1 : 0); index < input.Length; index++)
+                {
+                    var addedValue = (int)Math.Pow(10, (input.Length - 1 - index)) * (int)Char.GetNumericValue(input[index]);
+                    results += addedValue;
+                }
+            }
+
+            return isNegative ? -results : results;
+        }
+    }
+}
---
+++ ExamplePlaygroundTests/IntParserTests.cs
@@ -0,0 +1,51 @@
+﻿using ExamplePlayground;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace ExamplePlaygroundTests
+{
+    public class IntParserTests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("20", 20)]
+        [InlineData("33", 33)]
+        [InlineData("-1", -1)]
+        [InlineData("282930123", 282930123)]
+        public void ParseTest(string input, int expectedValue)
+        {
+            var results = IntParser.Parse(input);
+            results.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("a")]
+        [InlineData("$%")]
+        [InlineData("123%^^&3123")]
+        [InlineData("123%")]
+        [InlineData("-123%")]
+        public void ParseFaliureTests(string input)
+        {
+            var action = new Action(() => { IntParser.Parse(input); });
+            action.ShouldThrow<Exception>().WithMessage("Not valid input");
+        }
+
+        [Theory]
+        [InlineData("2147483648")]
+        [InlineData("-2147483648")]
+        public void ParseExtremesTests(string input)
+        {
+            try
+            {
+                IntParser.Parse(input);
+            }
+            catch (Exception exception)
+            {
+                exception.GetType().Should().Be<ArgumentException>();
+            }
+        }
+    }
+}
