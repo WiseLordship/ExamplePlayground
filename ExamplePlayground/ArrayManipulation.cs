ab7bb16d2ac7594b292d55f72593cf95af6e3c74
Lord <ION3395@absg.net>
2017-12-21 08:23:16 -0600

    Adding Array sort by low to high example

---
+++ ExamplePlayground/ArrayManipulation.cs
@@ -0,0 +1,26 @@
+﻿namespace ExamplePlayground
+{
+    public static class ArrayManipulation
+    {
+        public static int[] StortArrayLowHigh(int[] input)
+        {
+            if (input == null) { return null; }
+            if (input.Length == 1) { return input; }
+
+            for (var index = 0; index < input.Length; index++)
+            {
+                for (var secondaryIndex = index + 1; secondaryIndex < input.Length; secondaryIndex++)
+                {
+                    if (input[secondaryIndex] < input[index])
+                    {
+                        var holder = input[index];
+                        input[index] = input[secondaryIndex];
+                        input[secondaryIndex] = holder;
+                    }
+                }
+            }
+
+            return input;
+        }
+    }
+}
---
+++ ExamplePlaygroundTests/ArrayManipulationTests.cs
@@ -0,0 +1,22 @@
+﻿using ExamplePlayground;
+using FluentAssertions;
+using Xunit;
+namespace ExamplePlaygroundTests
+{
+    public class ArrayManipulationTests
+    {
+        [Theory]
+        [InlineData(new int[0], new int[0])]
+        [InlineData(null, null)]
+        [InlineData(new int[] { 1 }, new int[] { 1 })]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1, 2 })]
+        [InlineData(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { -1, 2, -421, 23 }, new int[] { -421, -1, 2, 23 })]
+        [InlineData(new int[] { 10, 304, 124, 1235, 342, 2313, 12, 3, 1 }, new int[] { 1, 3, 10, 12, 124, 304, 342, 1235, 2313 })]
+        public void ArrayManipulationTest(int[] input, int[] expected)
+        {
+            var result = ArrayManipulation.StortArrayLowHigh(input);
+            result.Should().Equal(expected);
+        }
+    }
+}
